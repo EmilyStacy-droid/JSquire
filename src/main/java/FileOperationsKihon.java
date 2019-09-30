09a779a73f1eb2d08874cf0fcd3bfe02cbae9c37
Emily Stacy <Emily.Stacy@improving.com>
2019-09-30 08:15:59 -0500

    first try

--- src/main/java/FileOperationsKihon.java
+++ src/main/java/FileOperationsKihon.java
@@ -1,16 +1,20 @@
 import framework.FileOperationsKihonBase;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class FileOperationsKihon extends FileOperationsKihonBase {
     @Override
     public String getContentsOfThisFile(Path pathOfFile) throws IOException {
-        throw new UnsupportedOperationException();
+        return Files.readString(Path pathOfFile);
     }
 
     @Override
     public void writeContentsToThisFile(Path pathOfFile, String contents) throws IOException {
-        throw new UnsupportedOperationException();
+        return Files.write(Path pathOfFile) {
+            Files.append(contents);
+        }
     }
 }
