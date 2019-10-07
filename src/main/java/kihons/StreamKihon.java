d3e12461f3190fa71f6c5f877915718db5149640
Emily Stacy <Emily.Stacy@improving.com>
2019-10-07 09:10:30 -0500

    final

--- src/main/java/kihons/StreamKihon.java
+++ src/main/java/kihons/StreamKihon.java
@@ -14,31 +14,44 @@ public class StreamKihon extends StreamsKihonBase {
 
     @Override
     protected List<String> getTheNamesOfAllItems(List<Item> items) {
-        return items.stream().map(i -> i.getName()).collect(Collectors.toList());
+        return items.stream()
+                .map(i -> i.getName())
+                .collect(Collectors.toList());
     }
 
     @Override
     protected List<Item> sortItemsAlphabeticallyBasedOnName(List<Item> items) {
-       return items.stream().map(i ->i.getName()).sorted().collect(Collectors.toList(Item));
+       return items.stream()
+               .sorted(Comparator.comparing(Item::getName))
+               .collect(Collectors.toList());
     }
 
     @Override
     protected List<Item> sortOnCostFromLestToMostExpensiveAllItemsThatCostLessThanOrEqualToMyMonies(List<Item> items, int monies) {
-       items.stream().sorted(Comparator.comparing(monies, )).
+       return items.stream()
+               .filter(i -> i.getCost() <= monies)
+               .sorted(Comparator.comparingInt(Item::getCost))
+               .collect(Collectors.toList());
     }
 
     @Override
     protected Item getTheItemWithTheMostCost(List<Item> items) {
-        throw new NotImplementedYetException();
+
+        return items.stream()
+                .max(Comparator.comparingInt(Item::getCost))
+                .orElseThrow();
     }
 
     @Override
     protected int getTheAverageCostRoundedDown(List<Item> items) {
-        throw new NotImplementedYetException();
+
+        return items.stream()
+                .mapToInt(i->i.getCost()).sum()/items.size();
     }
 
     @Override
     protected void addAllItemsToMyMapWIthKetNameAndValueOfCost(List<Item> items, Map<String, Integer> myMap) {
-        throw new NotImplementedYetException();
+        items.stream().
+                forEach(item -> myMap.put(item.getName(), item.getCost()));
     }
 }
